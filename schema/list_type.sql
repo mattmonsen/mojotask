a725832a4eddb22be920c34d4595c75dad121b0e
Matt Monsen <matt@mattmonsen.com>
2013-06-18 18:33:14 -0600

    Add new schema for making lists and add the route and header entries

--- schema/list_type.sql
+++ schema/list_type.sql
@@ -1,8 +1,10 @@
-CREATE TABLE `user` (
+CREATE TABLE `list_type` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) NOT NULL,
-  `password` varchar(32) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `user_id` int(10) unsigned NOT NULL DEFAULT 1,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `username` (`username`)
+  KEY `name` (`name`),
+  KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
