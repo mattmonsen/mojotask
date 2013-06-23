3031366490bb3423fbd48e6ecedab9dcb8ef7236
Matt Monsen <matt@mattmonsen.com>
2013-06-22 22:23:02 -0600

    Alter mojottask_list.sql to include the new key updates

--- schema/mojotask_list.sql
+++ schema/mojotask_list.sql
@@ -2,6 +2,7 @@ CREATE TABLE `mojotask_list` (
   `mojotask_id` int(10) unsigned,
   `list_id` int(10) unsigned,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`mojotask_id`, `list_id`)
+  PRIMARY KEY (`mojotask_id`),
+  KEY `list_id` (`list_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
 
